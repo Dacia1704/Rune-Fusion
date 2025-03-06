0a3e139d346462d309d3865ec24233a48c1fd478
Seichi04 <duongbacdinhthoa@gmail.com>
2025-03-07 01:39:49 +0700

    fix error not run in main thread

---
+++ Rune Fusion Client/Assets/Scripts/Network/SocketManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class SocketManager : MonoBehaviour
+{
+    private SocketIOUnity socket;
+
+    private MapData mapDataPayload;
+    
+    
+    void Start()
+    {
+        mapDataPayload = new MapData
+        {
+            cols = GameManager.Instance.GameManagerSO.Width,
+            rows = GameManager.Instance.GameManagerSO.Height,
+            numTypes = GameManager.Instance.RuneManager.RuneManagerSO.SingleRuneList.Count,
+        }; 
+        
+        socket = new SocketIOUnity("http://localhost:3000");
+        socket.OnConnected += (sender, e) =>
+        {
+            Debug.Log("Socket connected");
+            MapDataRequest();
+        };
+        
+        socket.On("genMapResponse", data =>
+        {
+            List<List<List<int>>> mapData = JsonConvert.DeserializeObject<List<List<List<int>>>>(data.ToString());
+            UnityMainThreadDispatcher.Instance().Enqueue(GenMapCoroutine(mapData[0]));
+        });
+        socket.Connect();
+        
+    }
+
+    
+    // Use coroutine to force this action performed in the main thead because some default function in unity must run in main thread
+    private IEnumerator GenMapCoroutine(List<List<int>> mapData)
+    {
+        GameManager.Instance.RuneManager.GenerateGrid(mapData);
+        GameManager.Instance.SetUpTilePosition();
+        yield return null;
+    }
+
+    private void MapDataRequest()
+    {
+        string jsonData = JsonUtility.ToJson(mapDataPayload);
+        socket.Emit("genMap", jsonData);
+    }
+    
+    
+}
--- Rune Fusion Client/Assets/Scripts/Runes/RuneManager.cs
+++ Rune Fusion Client/Assets/Scripts/Runes/RuneManager.cs
@@ -9,30 +9,33 @@ using UnityEngine;
 
 public class RuneManager : MonoBehaviour
 {
-    public GameObject TilePrefab;
-    public GameObject[,] Tiles {get;private set;}
+    [field: SerializeField] public RuneManagerSO RuneManagerSO { get; private set; }
+    public GameObject[,] Tiles {get;private set;} // list : start from bottom to top and from left to right;
     private float sizeTile;
-    
 
-    public void GenerateGrid()
+    public void GenerateGrid(List<List<int>> typesList)
     { 
+        GameManager.Instance.CheckIfMainThread();
         Tiles = new GameObject[GameManager.Instance.GameManagerSO.Height+2,GameManager.Instance.GameManagerSO.Width+2]; 
         sizeTile = CameraManager.Instance.GetWidthCamera()/ GameManager.Instance.GameManagerSO.Width;
-        for(int x=1;x<=GameManager.Instance.GameManagerSO.Width;x++)
+        for(int x=0;x<GameManager.Instance.GameManagerSO.Width;x++)
         {
-            for(int y=1;y<=GameManager.Instance.GameManagerSO.Height;y++)
+            for(int y=0;y<GameManager.Instance.GameManagerSO.Height;y++)
             {
-                GameObject tileObject = Instantiate(TilePrefab,new Vector2(0,0),Quaternion.identity);
+                GameObject tileObject = Instantiate(RuneManagerSO.SingleRuneList[typesList[y][x]],new Vector2(0,0),Quaternion.identity);
                 Rune rune = tileObject.GetComponent<Rune>();
                 rune.Col = x;
                 rune.Row = y;
-                tileObject.transform.position = new Vector2( x * sizeTile - sizeTile/2 - GameManager.Instance.GameManagerSO.Width*sizeTile/2,y * sizeTile - sizeTile/2 - GameManager.Instance.GameManagerSO.Height*sizeTile/2 );
+                tileObject.transform.position = new Vector2( (x+1) * sizeTile - sizeTile/2 - GameManager.Instance.GameManagerSO.Width*sizeTile/2,
+                    (y+1) * sizeTile - sizeTile/2 - GameManager.Instance.GameManagerSO.Height*sizeTile/2 );
                 tileObject.transform.parent = transform;
                 tileObject.name = $"tile {y}-{x}";
                 Tiles[y,x] = tileObject;
                 tileObject.transform.localScale = new Vector3(sizeTile,sizeTile,sizeTile);
             }
         }
+        
+        
     }
 
     public float GetHeightTiles()
