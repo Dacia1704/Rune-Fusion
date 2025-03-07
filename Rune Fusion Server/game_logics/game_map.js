59d720e11e63c28c7eb9851c8e6af6d36c8823c8
Seichi04 <duongbacdinhthoa@gmail.com>
2025-03-07 22:48:38 +0700

    update gen map

--- Rune Fusion Server/game_logics/game_map.js
+++ Rune Fusion Server/game_logics/game_map.js
@@ -1,14 +1,34 @@
 import getRandomInt from "../utils/random.js";
 
 export default function generateRuneMap(mapData) {
-  const map = Array.from({ length: mapData.rows }, (_, rowIndex) => {
+  const map = [];
+  for (let rowIndex = 0; rowIndex < mapData.rows; rowIndex++) {
     const row = [];
     for (let colIndex = 0; colIndex < mapData.cols; colIndex++) {
-      row.push(getRandomInt(0, mapData.numTypes - 1));
+      let newTile;
+      do {
+        newTile = getRandomInt(0, mapData.numTypes - 1);
+      } while (
+        (colIndex - 2 >= 0 &&
+          newTile === row[colIndex - 1] &&
+          newTile === row[colIndex - 2]) ||
+        (rowIndex - 2 >= 0 &&
+          newTile === map[rowIndex - 1][colIndex] &&
+          newTile === map[rowIndex - 2][colIndex])
+      );
+      row.push(newTile);
     }
 
-    return row;
-  });
+    map.push(row);
+  }
 
   return map;
+}
+
+export function generateNewRune(currentMap) {
+  const rows = currentMap.Count;
+  const cols = currentMap[0].Count;
+  const newRuneMap = Array.from({ length: mapData.rows }, () =>
+    Array(mapData.cols).fill(0)
+  );
 }
