ba731be1033f31deac2ebd31de2496044a9db8cb
Seichi04 <duongbacdinhthoa@gmail.com>
2025-04-17 23:36:46 +0700

    fix jwt secrect

--- RuneFusionServer/config/prod.js
+++ RuneFusionServer/config/prod.js
@@ -1,7 +1,7 @@
 const prodConfig = {
-  port: process.env.PORT,
-  mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.jwtSecret,
+    port: process.env.PORT,
+    mongoURI: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
 };
 
 export default prodConfig;
